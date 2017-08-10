aeaac7b65321c19ba5fb02c217070e4fc70a56d0
tony <anthony.kerz@gmail.com>
2017-08-09 23:43:03 -0400

    init

--- src/index.js
+++ src/index.js
@@ -7,9 +7,14 @@ const configuration = {}
 
 const clients = [
   {
+    application_type: 'web',
     client_id: 'foo',
     client_secret: 'bar',
-    redirect_uris: ['http://lvh.me:8080/cb']
+    // redirect_uris: ['https://lvh.me:8080/'],
+    redirect_uris: ['http://localhost:8080/'],
+    grant_types: ['implicit'],
+    token_endpoint_auth_method: 'none',
+    response_types: ['id_token']
   }
 ]
 
